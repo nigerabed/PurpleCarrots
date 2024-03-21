e84af6e97ec3d8359fb5a04d5f80febc6ac7018e
Niger <nigerabed@gmail.com>
2024-03-21 10:31:22 +0100

    autometic server open

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/Components/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
+    mode: "development",
+  entry: "./src/Components/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 500,
-        ignored: /node_modules/,
-        poll: 1000,
-    },
-    devtool: "source-map",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 500,
+    ignored: /node_modules/,
+    poll: 1000,
+  },
+  devtool: "source-map",
 };
